71fc6ecad9d7c9b678d15572082f0802ba08dde3
Pradeep <pradeepj@cannydigit.com>
2022-08-06 12:51:38 +0530

    job application one

--- src/job-application/job-application.service.ts
+++ src/job-application/job-application.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateJobApplicationInput } from './dto/create-job-application.input';
-import { UpdateJobApplicationInput } from './dto/update-job-application.input';
+// import { UpdateJobApplicationInput } from './dto/update-job-application.input';
 
 @Injectable()
 export class JobApplicationService {
@@ -18,29 +18,30 @@ export class JobApplicationService {
   async findAll() {
     return await this.prisma.jobApplication.findMany();
   }
+
+  async findOne(id: string) {
+    this.prisma.jobApplication.findFirst;
+    const jobApplication = await this.prisma.jobApplication.findUnique({
+      where: { id },
+    });
+    if (!jobApplication) {
+      return new NotFoundException('jobapplication not found');
+    }
+
+    //   async update(
+    //     id: string,
+    //     updateJobApplicationInput: UpdateJobApplicationInput,
+    //   ) {
+    //     const data: Prisma.JobApplicationUpdateInput = {
+    //       appliedJob: { connect: { id: updateJobApplicationInput.jobId } },
+    //       appliedBy: { connect: { id: updateJobApplicationInput.profileId } },
+    //     };
+    //     return await this.prisma.jobApplication.update({ where: { id }, data });
+    //   }
+
+    //   async remove(id: string) {
+    //     return await this.prisma.jobApplication.delete({ where: { id } });
+    //   }
+    // }
+  }
 }
-//   async findOne(id: string) {
-//     this.prisma.jobApplication.findFirst;
-//     const jobApplication = await this.prisma.jobApplication.findUnique({
-//       where: { id },
-//     });
-//     if (!jobApplication) {
-//       return new NotFoundException('jobapplication not found');
-//     }
-//   }
-
-//   async update(
-//     id: string,
-//     updateJobApplicationInput: UpdateJobApplicationInput,
-//   ) {
-//     const data: Prisma.JobApplicationUpdateInput = {
-//       appliedJob: { connect: { id: updateJobApplicationInput.jobId } },
-//       appliedBy: { connect: { id: updateJobApplicationInput.profileId } },
-//     };
-//     return await this.prisma.jobApplication.update({ where: { id }, data });
-//   }
-
-//   async remove(id: string) {
-//     return await this.prisma.jobApplication.delete({ where: { id } });
-//   }
-// }
